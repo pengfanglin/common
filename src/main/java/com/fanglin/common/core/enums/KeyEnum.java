cbb2b51e9f1533a729294907aecece587c42b728
pengfanglin <2639331561@qq.com>
2019-09-01 17:32:27 +0800

    /

---
+++ src/main/java/com/fanglin/common/core/enums/KeyEnum.java
@@ -0,0 +1,35 @@
+package com.fanglin.common.core.enums;
+
+/**
+ * 字符串参数约束
+ *
+ * @author 彭方林
+ * @version 1.0
+ * @date 2019/5/16 22:03
+ **/
+public interface KeyEnum {
+    /**
+     * 字符串
+     *
+     * @return
+     */
+    String getKey();
+
+    /**
+     * 根据枚举值查找对应的枚举类
+     *
+     * @param enumClass 枚举类
+     * @param code      枚举值
+     * @param <E>
+     * @return
+     */
+    static <E extends Enum<?> & KeyEnum> E find(Class<E> enumClass, String key) {
+        E[] enumConstants = enumClass.getEnumConstants();
+        for (E e : enumConstants) {
+            if (e.getKey().equals(key)) {
+                return e;
+            }
+        }
+        return null;
+    }
+}
